24b0048769909db38fd6db971d406df003964292
mohitsharma-coder <mohitsharma022004@gmail.com>
2025-10-09 14:27:15 +0530

    Time: 21 ms (84.19%) | Memory: 61.5 MB (62.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int, vector<int>, greater<int>> minHeap;
+
+//         for(int i=0;i<nums.size();i++){    //push number of element in minHeap box
+//             if(minHeap.size()< k){
+//                 minHeap.push(nums[i]);
+//             }
+//             else if(nums[i]>minHeap.top()){     //top always shows minHeap smallest element
+//                 minHeap.pop();                  //if top element is small than pop
+//                 minHeap.push(nums[i]);          //and new element is push
+//             }
+//         }
+//         return minHeap.top();
+//     }
+// };
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int, vector<int>, greater<int>> minHeap;
 
-        for(int i=0;i<nums.size();i++){    //push number of element in minHeap box
-            if(minHeap.size()< k){
-                minHeap.push(nums[i]);
-            }
-            else if(nums[i]>minHeap.top()){     //top always shows minHeap smallest element
-                minHeap.pop();                  //if top element is small than pop
-                minHeap.push(nums[i]);          //and new element is push
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.push(num);
+            } else if (num > minHeap.top()) {
+                minHeap.pop();
+                minHeap.push(num);
             }
         }
+
         return minHeap.top();
     }
 };
