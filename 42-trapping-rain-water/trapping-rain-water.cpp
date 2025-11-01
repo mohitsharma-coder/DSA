eb524d836b17e86967f1abf14c9db11ae8490f87
mohitsharma-coder <mohitsharma022004@gmail.com>
2025-11-01 19:56:06 +0530

    Time: 4 ms (11.18%) | Memory: 26 MB (60.75%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int leftmax = 0, rightmax = 0, total = 0;
+        int n = height.size();
+        int l=0, r=n-1;
+        int rmax = 0, lmax = 0, ans=0;
 
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= leftmax)
-                    leftmax = height[left];
-                else
-                    total += leftmax - height[left];
-                left++;
-            } else {
-                if (height[right] >= rightmax)
-                    rightmax = height[right];
-                else
-                    total += rightmax - height[right];
-                right--;
+        while(l<r){
+            //for boundary 
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
+
+            if(lmax<rmax){
+                ans += lmax-height[l];
+                l++;
+            }
+            else{
+                ans += rmax - height[r];
+                r--;
             }
         }
-        return total;
+        return ans;
     }
 };
+//  int left = 0, right = height.size() - 1;
+//         int leftmax = 0, rightmax = 0, total = 0;
+
+//         while (left < right) {
+//             if (height[left] <= height[right]) {
+//                 if (height[left] >= leftmax)
+//                     leftmax = height[left];
+//                 else
+//                     total += leftmax - height[left];
+//                 left++;
+//             } else {
+//                 if (height[right] >= rightmax)
+//                     rightmax = height[right];
+//                 else
+//                     total += rightmax - height[right];
+//                 right--;
+//             }
+//         }
+//         return total;
