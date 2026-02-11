d3a7dc4e47d11a3452ccf657276c2ebd35f1739b
mohitsharma-coder <mohitsharma022004@gmail.com>
2026-02-11 15:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 115.6 MB (66.57%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -3,11 +3,9 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int firstElement = INT_MAX;
         int secondElement = INT_MAX;
-        int thirdElement = INT_MAX;
 
-        for(int number : nums){                 //TAKE ALL VALUE WHICH PRESENT IN NUMS
+        for(int number : nums){            //TAKE ALL VALUE WHICH PRESENT IN NUMS
             
-
             if(firstElement>=number)
             firstElement = number;
 
