c80b12214dfde9d0c9b34f4cf461feaa92a49d1b
mohitsharma-coder <mohitsharma022004@gmail.com>
2026-02-25 10:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (53.58%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) { 
-        int totalsum = 0; //sum 
+        int totalsum = 0;  
         for(int i=0; i<nums.size();i++){
             totalsum += nums[i];
         }
